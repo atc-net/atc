4d67f4dbf996cb6f39fa9cbc45d3b673f8c09e68
Christian Helle <christian.helle@outlook.com>
2020-09-10 14:28:40 +0200

    Implement IConfigureOptions for SwaggerUIOptions

---
+++ src/Atc.Rest.Extended/Options/ConfigureSwaggerOptions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace Atc.Rest.Extended.Options
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerUIOptions>
+    {
+        private readonly IApiVersionDescriptionProvider versionDescriptionProvider;
+        private readonly RestApiExtendedOptions restApiOptions;
+
+        public ConfigureSwaggerOptions(
+            IApiVersionDescriptionProvider versionDescriptionProvider,
+            RestApiExtendedOptions restApiOptions)
+        {
+            this.versionDescriptionProvider = versionDescriptionProvider ?? throw new ArgumentNullException(nameof(versionDescriptionProvider));
+            this.restApiOptions = restApiOptions ?? throw new ArgumentNullException(nameof(restApiOptions));
+        }
+
+        public void Configure(SwaggerUIOptions options)
+        {
+            ConfigureSwaggerEndpointPerApiVersion(options);
+
+            options.EnableValidator();
+            options.DocExpansion(DocExpansion.List);
+            options.SupportedSubmitMethods(SubmitMethod.Get, SubmitMethod.Post, SubmitMethod.Put, SubmitMethod.Patch, SubmitMethod.Delete);
+            options.DefaultModelRendering(ModelRendering.Model);
+            options.DisplayRequestDuration();
+            options.EnableDeepLinking();
+            options.ShowCommonExtensions();
+            options.InjectStylesheet("/swagger-ui/style.css");
+            options.InjectJavascript("/swagger-ui/main.js");
+        }
+
+        private void ConfigureSwaggerEndpointPerApiVersion(SwaggerUIOptions options)
+        {
+            if (restApiOptions.UseApiVersioning)
+            {
+                foreach (var description in versionDescriptionProvider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint(
+                        $"/swagger/{description.GroupName}/swagger.json",
+                        description.GroupName.ToUpperInvariant());
+                }
+            }
+            else
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Default");
+            }
+        }
+    }
+}
