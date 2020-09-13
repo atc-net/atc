1756374b9c262ad3e9406e04ff5f70415011b9b2
Christian Helle <christian.helle@outlook.com>
2020-09-13 02:48:58 +0200

    Introduce Authorization properties in RestApiExtendedOptions

--- src/Atc.Rest.Extended/Options/RestApiExtendedOptions.cs
+++ src/Atc.Rest.Extended/Options/RestApiExtendedOptions.cs
@@ -1,4 +1,6 @@
-﻿using Atc.Rest.Options;
+﻿using System;
+using System.Collections.Generic;
+using Atc.Rest.Options;
 
 namespace Atc.Rest.Extended.Options
 {
@@ -9,5 +11,11 @@ namespace Atc.Rest.Extended.Options
         public bool UseFluentValidation { get; set; } = true;
 
         public bool UseOpenApiSpec { get; set; } = true;
+
+        public string AuthorizationTenant { get; set; } = string.Empty;
+
+        public string AuthorizationClientId { get; set; } = string.Empty;
+
+        public IReadOnlyCollection<string> AuthorizationValidAudiences { get; set; } = Array.Empty<string>();
     }
 }
