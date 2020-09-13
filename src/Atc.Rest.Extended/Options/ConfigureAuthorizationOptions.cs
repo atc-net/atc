fc0d73867f0afbb5cbc7554f36cee0319460219b
Christian Helle <christian.helle@outlook.com>
2020-09-13 02:50:28 +0200

    Implement IPostConfigureOptions for JwtBearerOptions and AuthenticationOptions

---
+++ src/Atc.Rest.Extended/Options/ConfigureAuthorizationOptions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Atc.Rest.Extended.Options
+{
+    public class ConfigureAuthorizationOptions :
+        IPostConfigureOptions<JwtBearerOptions>,
+        IPostConfigureOptions<AuthenticationOptions>
+    {
+        private readonly RestApiExtendedOptions apiOptions;
+
+        public ConfigureAuthorizationOptions(
+            RestApiExtendedOptions options)
+        {
+            apiOptions = options ?? throw new ArgumentNullException(nameof(options));
+        }
+
+        public void PostConfigure(string name, JwtBearerOptions options)
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            if (string.IsNullOrEmpty(apiOptions.AuthorizationTenant))
+            {
+                throw new InvalidOperationException("AuthorizationTenant not defined");
+            }
+
+            if (string.IsNullOrEmpty(apiOptions.AuthorizationClientId))
+            {
+                throw new InvalidOperationException("AuthorizationClientId not defined");
+            }
+
+            if (!apiOptions.AuthorizationValidAudiences.Any())
+            {
+                throw new InvalidOperationException("AuthorizationValidAudiences is empty");
+            }
+
+            options.Authority = $"https://login.microsoftonline.com/{apiOptions.AuthorizationTenant}/";
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = true,
+                ValidAudiences = apiOptions.AuthorizationValidAudiences,
+            };
+        }
+
+        public void PostConfigure(string name, AuthenticationOptions options)
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        }
+    }
+}
