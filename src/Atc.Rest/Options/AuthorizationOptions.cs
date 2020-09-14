696c06ded56ecc54aba3eb73df6b92b09bbe05b1
Christian Helle <christian.helle@outlook.com>
2020-09-14 16:45:29 +0200

    Add comments to AuthorizationOptions on how to use

--- src/Atc.Rest/Options/AuthorizationOptions.cs
+++ src/Atc.Rest/Options/AuthorizationOptions.cs
@@ -3,6 +3,30 @@ using System.Collections.Generic;
 
 namespace Atc.Rest.Options
 {
+    /// <summary>
+    /// Copy and fill out the AzureAd section into the project User Secrets
+    ///
+    /// <code>
+    /// {
+    ///   "AzureAd": {
+    ///     "Instance": "https://login.microsoftonline.com",
+    ///     "ClientId": "[Application ID of the Azure AD App Registration]",
+    ///     /*
+    ///       You need specify the TenantId only if you want to accept access tokens from a single tenant
+    ///      (line-of-business app).
+    ///       Otherwise, you can leave them set to common.
+    ///       This can be:
+    ///       - A GUID (Tenant ID = Directory ID)
+    ///       - 'common' (any organization and personal accounts)
+    ///       - 'organizations' (any organization)
+    ///       - 'consumers' (Microsoft personal accounts)
+    ///     */
+    ///     "TenantId": "common",
+    ///     "Audience": "[App Identifier URI of the Azure AD App Registration]"
+    ///   }
+    /// }
+    /// </code>
+    /// </summary>
     public class AuthorizationOptions
     {
         public const string ConfigurationSectionName = "AzureAd";
