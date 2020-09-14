2e6b0b649f2633a8f1ff6bbc3f7872942562d4a5
Christian Helle <christian.helle@outlook.com>
2020-09-14 12:20:24 +0200

    Refactor AuthorizationOptions class out of RestApiExtendedOptions

--- src/Atc.Rest.Extended/Options/RestApiExtendedOptions.cs
+++ src/Atc.Rest.Extended/Options/RestApiExtendedOptions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Atc.Rest.Options;
+﻿using Atc.Rest.Options;
 
 namespace Atc.Rest.Extended.Options
 {
@@ -11,11 +9,5 @@ namespace Atc.Rest.Extended.Options
         public bool UseFluentValidation { get; set; } = true;
 
         public bool UseOpenApiSpec { get; set; } = true;
-
-        public string AuthorizationTenant { get; set; } = string.Empty;
-
-        public string AuthorizationClientId { get; set; } = string.Empty;
-
-        public IReadOnlyCollection<string> AuthorizationValidAudiences { get; set; } = Array.Empty<string>();
     }
 }
---
+++ src/Atc.Rest/Options/AuthorizationOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atc.Rest.Options
+{
+    public class AuthorizationOptions
+    {
+        public const string ConfigurationSectionName = "AzureAd";
+
+        public bool UseAzureADBearer { get; set; }
+
+        public string ClientId { get; set; } = string.Empty;
+
+        public string TenantId { get; set; } = string.Empty;
+
+        public string Instance { get; set; } = string.Empty;
+
+        public string Domain { get; set; } = string.Empty;
+
+        public string Audience { get; set; } = string.Empty;
+
+        public IReadOnlyCollection<string> ValidAudiences { get; set; } = Array.Empty<string>();
+    }
+}
