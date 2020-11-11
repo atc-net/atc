af4b953dc15e865c096eb1f0db29faeddaad76cc
Per Kops <pko@delegate.dk>
2020-11-11 10:45:00 +0100

    Ensure proper generation of route in Controllers and in EndpointTests

---
+++ sample/Demo.Domain/Handlers/RouteWithDash/GetRouteWithDashHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Demo.Api.Generated.Contracts.RouteWithDash;
+
+namespace Demo.Domain.Handlers.RouteWithDash
+{
+    /// <summary>
+    /// Handler for operation request.
+    /// Description: Your GET endpoint.
+    /// Operation: GetRouteWithDash.
+    /// Area: RouteWithDash.
+    /// </summary>
+    public class GetRouteWithDashHandler : IGetRouteWithDashHandler
+    {
+        public Task<GetRouteWithDashResult> ExecuteAsync(CancellationToken cancellationToken = default)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
