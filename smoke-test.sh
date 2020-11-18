71be9bb1b49f58870d193783825a660fed45af1c
Christian Helle <crh@delegate.dk>
2020-11-18 01:14:04 +0100

    Update smoke tests to also test against Swagger Petstore v2

--- smoke-test.sh
+++ smoke-test.sh
@@ -5,9 +5,9 @@ curl -O https://petstore3.swagger.io/api/v3/openapi.yaml
 
 echo "Generate code"
 cd src/Atc.Rest.ApiGenerator.CLI
-dotnet run -- generate server all -p "Swagger Petstore v3" -s ../../openapi.yaml --outputSlnPath ../../petstore/ --outputSrcPath ../../petstore/src --outputTestPath ../../petstore/test -v true
+dotnet run -- generate server all -p "Swagger Petstore v3" -s ../../openapi.yaml --outputSlnPath ../../petstore3/ --outputSrcPath ../../petstore3/src --outputTestPath ../../petstore3/test -v true
 cd ../../
-dotnet build ./petstore
+dotnet build ./petstore3
 
-rm -rf ./petstore
+rm -rf ./petstore3
 rm openapi.yaml
