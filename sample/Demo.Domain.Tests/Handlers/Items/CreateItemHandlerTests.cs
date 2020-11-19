9f56a765741d15004fb6110460f49d86018ae00c
Claus Jakobsen <cja@delegate.dk>
2020-11-19 08:17:57 +0100

    Generate Demo project

---
+++ sample/Demo.Domain.Tests/Handlers/Items/CreateItemHandlerTests.cs
@@ -0,0 +1,18 @@
+﻿using Demo.Domain.Handlers.Items;
+using Xunit;
+
+namespace Demo.Domain.Tests.Handlers.Items
+{
+    public class CreateItemHandlerTests
+    {
+        [Fact(Skip="Change this to a real test")]
+        public void Sample()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
---
+++ sample/Demo.Domain.Tests/Handlers/Items/UpdateItemHandlerTests.cs
@@ -0,0 +1,18 @@
+﻿using Demo.Domain.Handlers.Items;
+using Xunit;
+
+namespace Demo.Domain.Tests.Handlers.Items
+{
+    public class UpdateItemHandlerTests
+    {
+        [Fact(Skip="Change this to a real test")]
+        public void Sample()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
