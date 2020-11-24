9a45e04dddd21cf2ee937821db4c273ad7cb2101
Thomas Malowanczyk <tkm@delegate.dk>
2020-11-24 10:56:26 +0100

    Generate demo project with new changes to WebApiStartupFactory Extend WebApiStartupFactory.Custom with example

--- sample/Demo.Api.Tests/WebApiStartupFactory.Custom.cs
+++ sample/Demo.Api.Tests/WebApiStartupFactory.Custom.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Demo.Api.Tests
 {
@@ -12,8 +13,15 @@ namespace Demo.Api.Tests
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Needed to show as an example.")]
         partial void ModifyConfiguration(IConfigurationBuilder config)
         {
-            // Modify configuration of generated class. 
+            // Modify configuration of generated class.
             // Can be used to remove e.g. Key Vault configured in Startup.
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Needed to show as an example.")]
+        partial void ModifyServices(IServiceCollection services)
+        {
+            // Modify services of generated class.
+            // Can be used to add e.g. mocked dependencies in Startup.
         }
     }
 }
