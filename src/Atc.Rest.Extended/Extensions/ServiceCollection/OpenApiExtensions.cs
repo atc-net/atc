d1a6a5aef1bf455f11439fccde8df567c343d4e5
Christian Helle <christian.helle@outlook.com>
2021-01-29 11:34:49 +0100

    Deprecate configuring SwaggerGenOptions using AddSwaggerGen(Action<SwaggerGenOptions>) as this prevents further customization

--- src/Atc.Rest.Extended/Extensions/ServiceCollection/OpenApiExtensions.cs
+++ src/Atc.Rest.Extended/Extensions/ServiceCollection/OpenApiExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using Atc.Rest.Extended.Filters;
 using Atc.Rest.Extended.Options;
@@ -10,69 +10,26 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class OpenApiExtensions
     {
+        [Obsolete]
         public static IServiceCollection AddOpenApiSpec<TStartup>(this IServiceCollection services)
         {
-            return services.AddOpenApiSpec<TStartup>(new RestApiExtendedOptions(), _ => { });
+            return services;
         }
 
+        [Obsolete]
         public static IServiceCollection AddOpenApiSpec<TStartup>(
             this IServiceCollection services,
             RestApiExtendedOptions restApiOptions)
         {
-            return services.AddOpenApiSpec<TStartup>(restApiOptions, _ => { });
+            return services;
         }
 
+        [Obsolete]
         public static IServiceCollection AddOpenApiSpec<TStartup>(
             this IServiceCollection services,
             RestApiExtendedOptions restApiOptions,
             Action<SwaggerGenOptions> setupAction)
         {
-            services.AddSwaggerGen(options =>
-            {
-                options.TagActionsBy(api =>
-                {
-                    if (api.ActionDescriptor is ControllerActionDescriptor controllerActionDescriptor)
-                    {
-                        return new[] { controllerActionDescriptor.ControllerName };
-                    }
-
-                    if (api.HttpMethod != null)
-                    {
-                        return new[] { api.HttpMethod };
-                    }
-
-                    throw new InvalidOperationException("Unable to determine tag for endpoint.");
-                });
-
-                options.OrderActionsBy((apiDesc) => $"{apiDesc.ActionDescriptor.RouteValues["controller"]}_{apiDesc.HttpMethod}");
-                options.IgnoreObsoleteActions();
-                options.IgnoreObsoleteProperties();
-                options.DefaultResponseForSecuredOperations();
-                options.TreatBadRequestAsDefaultResponse();
-
-                if (!restApiOptions.AllowAnonymousAccessForDevelopment)
-                {
-                    options.OperationFilter<SecurityRequirementsOperationFilter>();
-                }
-
-                options.DocumentFilter<SwaggerEnumDescriptionsDocumentFilter>();
-
-                if (restApiOptions.UseApiVersioning)
-                {
-                    options.ApplyApiVersioningFilters();
-                    options.ApplyApiVersioningSwaggerDocuments(services, typeof(TStartup).GetApiName());
-                }
-
-                options.IncludeXmlComments(Path.ChangeExtension(typeof(TStartup).Assembly.Location, "xml"));
-
-                foreach (var assemblyPairOptions in restApiOptions.AssemblyPairs)
-                {
-                    options.IncludeXmlComments(Path.ChangeExtension(assemblyPairOptions.ApiAssembly.Location, "xml"));
-                }
-
-                setupAction(options);
-            });
-
             return services;
         }
     }
