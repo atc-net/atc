80ee2d4469f58c22c2a6e706d7bd6689e9042c3d
David Kallesen <david@lmdk.dk>
2021-09-19 18:07:18 +0200

    Added overload parameter "timeoutInSec" for Execute-method and Kill-methods

--- src/Atc/Helpers/ProcessHelper.cs
+++ src/Atc/Helpers/ProcessHelper.cs
@@ -2,6 +2,9 @@ using System;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Atc.Helpers
@@ -22,6 +25,129 @@ namespace Atc.Helpers
             }
 
             return InvokeExecute(fileInfo, arguments);
+        }
+
+        public static Task<(bool isSuccessful, string output)> Execute(FileInfo fileInfo, string arguments, int timeoutInSec, CancellationToken cancellationToken = default)
+        {
+            if (fileInfo is null)
+            {
+                throw new ArgumentNullException(nameof(fileInfo));
+            }
+
+            if (arguments is null)
+            {
+                throw new ArgumentNullException(nameof(arguments));
+            }
+
+            return InvokeExecuteWithTimeout(fileInfo, arguments, timeoutInSec, cancellationToken);
+        }
+
+        public static (bool isSuccessful, string output) KillEntryCaller(int timeoutInSec = 30)
+        {
+            var processName = Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly()!.Location);
+            return KillByName(processName, allowMultiKill: true, timeoutInSec);
+        }
+
+        public static (bool isSuccessful, string output) KillById(int processId, int timeoutInSec = 30)
+        {
+            try
+            {
+                var process = Process.GetProcessById(processId);
+
+                // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+                if (process is null)
+                {
+                    return (
+                        isSuccessful: false,
+                        output: $"No process found by pid '{processId}'");
+                }
+
+                process.Kill();
+                process.WaitForExit(timeoutInSec * 1000);
+                return (
+                    isSuccessful: true,
+                    output: $"Process found by pid '{processId}' is now terminated");
+            }
+            catch (Exception ex)
+            {
+                return (
+                    isSuccessful: false,
+                    output: ex.GetMessage(
+                        includeInnerMessage: true,
+                        includeExceptionName: false));
+            }
+        }
+
+        public static (bool isSuccessful, string output) KillByName(string processName, bool allowMultiKill = true, int timeoutInSec = 30)
+        {
+            if (processName == null)
+            {
+                throw new ArgumentNullException(nameof(processName));
+            }
+
+            var processes = Process.GetProcessesByName(processName);
+            if (allowMultiKill)
+            {
+                if (processes.Length == 0)
+                {
+                    return (
+                        isSuccessful: false,
+                        output: $"No process found by name '{processName}'");
+                }
+
+                try
+                {
+                    foreach (var process in processes)
+                    {
+                        process.Kill();
+                        process.WaitForExit(timeoutInSec * 1000);
+                    }
+
+                    return (
+                        isSuccessful: true,
+                        output: $"{processes.Length} processes found by name '{processName}' is now terminated");
+                }
+                catch (Exception ex)
+                {
+                    return (
+                        isSuccessful: false,
+                        output: ex.GetMessage(
+                            includeInnerMessage: true,
+                            includeExceptionName: false));
+                }
+            }
+
+            switch (processes.Length)
+            {
+                case 0:
+                    return (
+                        isSuccessful: false,
+                        output: $"No process found by name '{processName}'");
+                case > 1:
+                    return (
+                        isSuccessful: false,
+                        output: $"Too many processes found by name '{processName}'");
+                default:
+                {
+                    var process = processes.First();
+                    try
+                    {
+                        process.Kill();
+                        process.WaitForExit(timeoutInSec * 1000);
+                        return (
+                            isSuccessful: true,
+                            output: $"Process found by name '{processName}' is now terminated");
+                    }
+                    catch (Exception ex)
+                    {
+                        return (
+                            isSuccessful: false,
+                            output: ex.GetMessage(
+                                includeInnerMessage: true,
+                                includeExceptionName: false));
+                    }
+                }
+            }
         }
 
         [SuppressMessage("Major Code Smell", "S3358:Ternary operators should not be nested", Justification = "OK.")]
@@ -53,15 +179,57 @@ namespace Atc.Helpers
                         ? standardError
                         : $"{standardOutput}{Environment.NewLine}{standardError}";
 
-                return (string.IsNullOrEmpty(standardError), message);
+                return (
+                    isSuccessful: string.IsNullOrEmpty(standardError),
+                    output: message);
             }
             catch (Exception ex)
             {
                 return (
                     isSuccessful: false,
-                    ex.GetMessage(
+                    output: ex.GetMessage(
                         includeInnerMessage: true,
                         includeExceptionName: true));
+            }
+        }
+
+        private static async Task<(bool isSuccessful, string output)> InvokeExecuteWithTimeout(FileSystemInfo fileInfo, string arguments, int timeoutInSec, CancellationToken cancellationToken)
+        {
+            var resultOutput = string.Empty;
+            var processName = Path.GetFileNameWithoutExtension(fileInfo.FullName);
+
+            try
+            {
+                var result = await TaskHelper
+                    .Execute(
+                        _ =>
+                        InvokeExecute(fileInfo, arguments),
+                        TimeSpan.FromSeconds(timeoutInSec),
+                        cancellationToken)
+                    .ConfigureAwait(false);
+
+                resultOutput = result.output;
+
+                KillByName(processName);
+
+                return result;
+            }
+            catch (TimeoutException)
+            {
+                var (killIsSuccessful, _) = KillByName(processName);
+
+                if (string.IsNullOrEmpty(resultOutput))
+                {
+                    resultOutput = killIsSuccessful
+                        ? $"Process has been running for {timeoutInSec}sec. before terminated."
+                        : $"Process has been running for {timeoutInSec}sec.";
+                }
+
+                return await Task
+                    .FromResult((
+                        isSuccessful: true,
+                        output: resultOutput))
+                    .ConfigureAwait(false);
             }
         }
     }
