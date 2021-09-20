7a6bd080e270e205b2e705dd4996057adf8d1d6e
David Kallesen <david@lmdk.dk>
2021-09-20 22:55:02 +0200

    Fix wrong assembly naming from GetExecutingAssembly to GetEntryAssembly

--- src/Atc.Console.Spectre/Factories/CommandAppFactory.cs
+++ src/Atc.Console.Spectre/Factories/CommandAppFactory.cs
@@ -22,7 +22,7 @@ namespace Atc.Console.Spectre.Factories
             var commandApp = new CommandApp(registrar);
 
             var appName = Assembly
-                .GetExecutingAssembly()
+                .GetEntryAssembly()!
                 .GetName()
                 .Name;
 
