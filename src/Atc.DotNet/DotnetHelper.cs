6ee9b63afbb1a3ace3e705769b4ea7ac69cb99b7
lupusbytes <bertram@lupusbytes.com>
2021-09-24 17:09:14 +0200

    Add attempt to get DotnetDirectory from environment variables if not found in first attempt

--- src/Atc.DotNet/DotnetHelper.cs
+++ src/Atc.DotNet/DotnetHelper.cs
@@ -16,13 +16,22 @@ namespace Atc.DotNet
         {
             var directory = new DirectoryInfo(RuntimeEnvironment.GetRuntimeDirectory());
 
-            while (directory.Parent is not null &&
-                  !directory.Name.Equals("dotnet", StringComparison.OrdinalIgnoreCase))
+            while (directory.Parent is not null && !IsDefaultDotnetDirectoryName(directory.Name))
             {
                 directory = directory.Parent;
             }
 
-            return directory;
+            if (directory.Exists && IsDefaultDotnetDirectoryName(directory.Name))
+            {
+                return directory;
+            }
+
+            if (TryGetDotnetDirectoryFromEnv("DOTNET_ROOT", out var result))
+            {
+                return result!;
+            }
+
+            throw new DirectoryNotFoundException("Could not determine location of system dotnet folder");
         }
 
         /// <summary>
@@ -34,13 +43,37 @@ namespace Atc.DotNet
         public static FileInfo GetDotnetExecutable()
         {
             var dotnetDirectory = GetDotnetDirectory();
-            var files = Directory.GetFiles(dotnetDirectory.FullName, "dotnet*");
+
+            var executableFile = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                ? "dotnet.exe"
+                : "dotnet";
+
+            var files = Directory.GetFiles(dotnetDirectory.FullName, executableFile);
             return files.Length switch
             {
                 0 => throw new FileNotFoundException($"Could not find a dotnet file in path '{dotnetDirectory.FullName}'."),
                 > 1 => throw new NotSupportedException($"Too many files matching dotnet* found in path '{dotnetDirectory.FullName}'."),
                 _ => new FileInfo(files[0]),
             };
+        }
+
+        private static bool IsDefaultDotnetDirectoryName(string value)
+        {
+            return value.Equals("dotnet", StringComparison.Ordinal) ||
+                   value.Equals(".dotnet", StringComparison.Ordinal);
+        }
+
+        private static bool TryGetDotnetDirectoryFromEnv(string envVariable, out DirectoryInfo? dotnetDirectory)
+        {
+            dotnetDirectory = null;
+            var dotnetRootEnv = Environment.GetEnvironmentVariable(envVariable);
+            if (!string.IsNullOrEmpty(dotnetRootEnv) && Directory.Exists(dotnetRootEnv))
+            {
+                dotnetDirectory = new DirectoryInfo(dotnetRootEnv);
+                return true;
+            }
+
+            return false;
         }
     }
 }
