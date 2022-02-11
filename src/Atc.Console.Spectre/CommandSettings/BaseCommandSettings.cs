c1401486e6515bab2420912649ce9a95b8bdee6d
David Kallesen <david@lmdk.dk>
2022-02-11 23:40:09 +0100

    Change IsOptionValueTrue from static to non-static - since it will only be used in a non-static context

--- src/Atc.Console.Spectre/CommandSettings/BaseCommandSettings.cs
+++ src/Atc.Console.Spectre/CommandSettings/BaseCommandSettings.cs
@@ -6,7 +6,8 @@ public class BaseCommandSettings : global::Spectre.Console.Cli.CommandSettings
     [Description("Use verbose for more debug/trace information")]
     public bool Verbose { get; set; }
 
-    public static bool IsOptionValueTrue(
+    [SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "OK. Will be used in a none-static context.")]
+    public bool IsOptionValueTrue(
         bool? value)
         => value is not null &&
            value.Value;
