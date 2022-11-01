796e3e353d126c12eca46fc96f2c662079d5fba8
Per Kops <pko@delegate.dk>
2022-11-01 11:20:35 +0100

    feat: Introducing new FromUnixTimeMs in LongExtensions

--- test/Atc.Tests/Extensions/BaseTypes/LongExtensionsTests.cs
+++ test/Atc.Tests/Extensions/BaseTypes/LongExtensionsTests.cs
@@ -17,4 +17,20 @@ public class LongExtensionsTests
         // Assert
         Assert.Equal(expectedDateTimeOffset, actual);
     }
+
+    [Theory]
+    [InlineData(1, 0, 0, 0, 0)]
+    [InlineData(1, 0, 0, 10, 10000)]
+    [InlineData(12, 13, 46, 40, 1000000000)]
+    public void FromUnixTimeMs(int expectedDay, int expectedHour, int expectedMinute, int expectedSecond, long input)
+    {
+        // Arrange
+        var expectedDateTimeOffset = new DateTimeOffset(1970, 1, expectedDay, expectedHour, expectedMinute, expectedSecond, new TimeSpan(0, 0, 0, 0, 0));
+
+        // Act
+        var actual = input.FromUnixTimeMs();
+
+        // Assert
+        Assert.Equal(expectedDateTimeOffset, actual);
+    }
 }
