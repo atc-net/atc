ae1c4ff229a7199de5778c7c09fa8b4b71cd5aea
David Kallesen <david@lmdk.dk>
2022-11-29 01:33:18 +0100

    fix: UT for DateTime extensions for GetPrettyTimeDiff

--- test/Atc.Tests/XUnitTestData/TestMemberDataForDateTimeExtensions.cs
+++ test/Atc.Tests/XUnitTestData/TestMemberDataForDateTimeExtensions.cs
@@ -1,80 +1,96 @@
+// ReSharper disable StringLiteralTypo
 namespace Atc.Tests.XUnitTestData;
 
 internal static class TestMemberDataForDateTimeExtensions
 {
-    public static TheoryData<string, DateTime> GetPrettyTimeDiff()
-    {
-        var unitTestEnd = DateTime.Now;
-
-        return new TheoryData<string, DateTime>
-        {
-            { "11,509 days", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15) },
-            { "12,213 hours", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15) },
-            { "13,234 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15) },
-            { "14,015 sec", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15) },
-            { "15,000 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15) },
-        };
-    }
-
-    public static TheoryData<string, DateTime, int> GetPrettyTimeDiffWithDecimalPrecision()
+    public static TheoryData<string, DateTime, int> GetPrettyTimeDiff()
     {
         var unitTestEnd = DateTime.Now;
 
         return new TheoryData<string, DateTime, int>
         {
-            { "11,5 days", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), 1 },
-            { "12,2 hours", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15), 1 },
-            { "13,2 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), 1 },
-            { "14,0 sec", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15), 1 },
-            { "15,0 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15), 1 },
-            { "11,51 days", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), 2 },
-            { "12,21 hours", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15), 2 },
-            { "13,23 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), 2 },
-            { "14,02 sec", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15), 2 },
-            { "15,00 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15), 2 },
-            { "11,509 days", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), 3 },
-            { "12,213 hours", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15), 3 },
-            { "13,234 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), 3 },
-            { "14,015 sec", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15), 3 },
-            { "15,000 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15), 3 },
+            //{ "11.509 days", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), GlobalizationLcidConstants.UnitedStates },
+            //{ "12.213 hours", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15), GlobalizationLcidConstants.UnitedStates },
+            //{ "13.234 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), GlobalizationLcidConstants.UnitedStates },
+            //{ "14.015 sec", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15), GlobalizationLcidConstants.UnitedStates },
+            //{ "15.000 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15), GlobalizationLcidConstants.UnitedStates },
+            //{ "11.509 days", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), GlobalizationLcidConstants.GreatBritain },
+            //{ "12.213 hours", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15), GlobalizationLcidConstants.GreatBritain },
+            //{ "13.234 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), GlobalizationLcidConstants.GreatBritain },
+            //{ "14.015 sec", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15), GlobalizationLcidConstants.GreatBritain },
+            //{ "15.000 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15), GlobalizationLcidConstants.GreatBritain },
+            //{ "11,509 dage", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), GlobalizationLcidConstants.Denmark },
+            //{ "12,213 timer", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15), GlobalizationLcidConstants.Denmark },
+            //{ "13,234 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), GlobalizationLcidConstants.Denmark },
+            //{ "14,015 sek", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15), GlobalizationLcidConstants.Denmark },
+            //{ "15,000 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15), GlobalizationLcidConstants.Denmark },
+            { "11,509 tage", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), GlobalizationLcidConstants.Germany },
+            { "12,213 stunden", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15), GlobalizationLcidConstants.Germany },
+            { "13,234 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), GlobalizationLcidConstants.Germany },
+            { "14,015 sek", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15), GlobalizationLcidConstants.Germany },
+            { "15,000 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15), GlobalizationLcidConstants.Germany },
         };
     }
 
-    public static TheoryData<string, DateTime, DateTime> GetPrettyTimeDiffWithEnd()
+    public static TheoryData<string, DateTime, int, int> GetPrettyTimeDiffWithDecimalPrecision()
     {
         var unitTestEnd = DateTime.Now;
 
-        return new TheoryData<string, DateTime, DateTime>
+        return new TheoryData<string, DateTime, int, int>
         {
-            { "11,509 days", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd },
-            { "12,213 hours", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15), unitTestEnd },
-            { "13,234 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd },
-            { "14,015 sec", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd },
-            { "15,000 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15), unitTestEnd },
+            { "11.5 days", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), 1, GlobalizationLcidConstants.UnitedStates },
+            { "12.2 hours", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15), 1, GlobalizationLcidConstants.UnitedStates },
+            { "13.2 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), 1, GlobalizationLcidConstants.UnitedStates },
+            { "14.0 sec", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15), 1 , GlobalizationLcidConstants.UnitedStates},
+            { "15.0 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15), 1, GlobalizationLcidConstants.UnitedStates },
+            { "11.51 days", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), 2, GlobalizationLcidConstants.UnitedStates },
+            { "12.21 hours", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15), 2 , GlobalizationLcidConstants.UnitedStates},
+            { "13.23 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), 2, GlobalizationLcidConstants.UnitedStates },
+            { "14.02 sec", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15), 2, GlobalizationLcidConstants.UnitedStates },
+            { "15.00 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15), 2, GlobalizationLcidConstants.UnitedStates },
+            { "11.509 days", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), 3, GlobalizationLcidConstants.UnitedStates },
+            { "12.213 hours", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15), 3, GlobalizationLcidConstants.UnitedStates },
+            { "13.234 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), 3, GlobalizationLcidConstants.UnitedStates },
+            { "14.015 sec", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15), 3, GlobalizationLcidConstants.UnitedStates },
+            { "15.000 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15), 3, GlobalizationLcidConstants.UnitedStates },
         };
     }
 
-    public static TheoryData<string, DateTime, DateTime, int> GetPrettyTimeDiffWithEndNowAndDecimalPrecision()
+    public static TheoryData<string, DateTime, DateTime, int> GetPrettyTimeDiffWithEnd()
     {
         var unitTestEnd = DateTime.Now;
 
         return new TheoryData<string, DateTime, DateTime, int>
         {
-            { "11,5 days", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, 1 },
-            { "12,2 hours", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15), unitTestEnd, 1 },
-            { "13,2 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, 1 },
-            { "14,0 sec", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, 1 },
-            { "15,0 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15), unitTestEnd, 1 },
-            { "11,51 days", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, 2 },
-            { "12,21 hours", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15), unitTestEnd, 2 },
-            { "13,23 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, 2 },
-            { "14,02 sec", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, 2 },
-            { "15,00 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15), unitTestEnd, 2 },
-            { "11,509 days", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, 3 },
-            { "12,213 hours", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15), unitTestEnd, 3 },
-            { "13,234 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, 3 },
-            { "14,015 sec", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, 3 },
-            { "15,000 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15), unitTestEnd, 3 },
+            { "11.509 days", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, GlobalizationLcidConstants.UnitedStates },
+            { "12.213 hours", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15), unitTestEnd, GlobalizationLcidConstants.UnitedStates },
+            { "13.234 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, GlobalizationLcidConstants.UnitedStates },
+            { "14.015 sec", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, GlobalizationLcidConstants.UnitedStates },
+            { "15.000 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15), unitTestEnd, GlobalizationLcidConstants.UnitedStates },
+        };
+    }
+
+    public static TheoryData<string, DateTime, DateTime, int, int> GetPrettyTimeDiffWithEndNowAndDecimalPrecision()
+    {
+        var unitTestEnd = DateTime.Now;
+
+        return new TheoryData<string, DateTime, DateTime, int, int>
+        {
+            { "11.5 days", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, 1, GlobalizationLcidConstants.UnitedStates },
+            { "12.2 hours", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15), unitTestEnd, 1, GlobalizationLcidConstants.UnitedStates },
+            { "13.2 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, 1, GlobalizationLcidConstants.UnitedStates },
+            { "14.0 sec", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, 1, GlobalizationLcidConstants.UnitedStates },
+            { "15.0 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15), unitTestEnd, 1, GlobalizationLcidConstants.UnitedStates },
+            { "11.51 days", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, 2, GlobalizationLcidConstants.UnitedStates },
+            { "12.21 hours", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15), unitTestEnd, 2, GlobalizationLcidConstants.UnitedStates },
+            { "13.23 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, 2, GlobalizationLcidConstants.UnitedStates },
+            { "14.02 sec", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, 2, GlobalizationLcidConstants.UnitedStates },
+            { "15.00 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15), unitTestEnd, 2, GlobalizationLcidConstants.UnitedStates },
+            { "11.509 days", new DateTime(unitTestEnd.Ticks).AddDays(-11).AddHours(-12).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, 3, GlobalizationLcidConstants.UnitedStates },
+            { "12.213 hours", new DateTime(unitTestEnd.Ticks).AddHours(-12).AddMinutes(-13).AddSeconds(14).AddMilliseconds(-15), unitTestEnd, 3, GlobalizationLcidConstants.UnitedStates },
+            { "13.234 min", new DateTime(unitTestEnd.Ticks).AddMinutes(-13).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, 3, GlobalizationLcidConstants.UnitedStates },
+            { "14.015 sec", new DateTime(unitTestEnd.Ticks).AddSeconds(-14).AddMilliseconds(-15), unitTestEnd, 3, GlobalizationLcidConstants.UnitedStates },
+            { "15.000 ms", new DateTime(unitTestEnd.Ticks).AddMilliseconds(-15), unitTestEnd, 3, GlobalizationLcidConstants.UnitedStates },
         };
     }
 }
