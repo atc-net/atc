f9c18c43f1c57b6e30761209410fd400216bb2d9
David Kallesen <david@lmdk.dk>
2022-11-29 01:14:21 +0100

    fix: CS8633 and S4457

--- test/Atc.Tests/XUnitTestData/TestMemberDataForTimeSpanExtensions.cs
+++ test/Atc.Tests/XUnitTestData/TestMemberDataForTimeSpanExtensions.cs
@@ -11,19 +11,16 @@ internal static class TestMemberDataForTimeSpanExtensions
             { "13,234 min", new TimeSpan(0, 0, 13, 14, 15), GlobalizationLcidConstants.UnitedStates },
             { "14,015 sec", new TimeSpan(0, 0, 0, 14, 15), GlobalizationLcidConstants.UnitedStates },
             { "15,000 ms", new TimeSpan(0, 0, 0, 0, 15), GlobalizationLcidConstants.UnitedStates },
-
             { "11,509 days", new TimeSpan(11, 12, 13, 14, 15), GlobalizationLcidConstants.GreatBritain },
             { "12,213 hours", new TimeSpan(0, 12, 13, 14, 15), GlobalizationLcidConstants.GreatBritain },
             { "13,234 min", new TimeSpan(0, 0, 13, 14, 15), GlobalizationLcidConstants.GreatBritain },
             { "14,015 sec", new TimeSpan(0, 0, 0, 14, 15), GlobalizationLcidConstants.GreatBritain },
             { "15,000 ms", new TimeSpan(0, 0, 0, 0, 15), GlobalizationLcidConstants.GreatBritain },
-
             { "11,509 dage", new TimeSpan(11, 12, 13, 14, 15), GlobalizationLcidConstants.Denmark },
             { "12,213 timer", new TimeSpan(0, 12, 13, 14, 15), GlobalizationLcidConstants.Denmark },
             { "13,234 min", new TimeSpan(0, 0, 13, 14, 15), GlobalizationLcidConstants.Denmark },
             { "14,015 sek", new TimeSpan(0, 0, 0, 14, 15), GlobalizationLcidConstants.Denmark },
             { "15,000 ms", new TimeSpan(0, 0, 0, 0, 15), GlobalizationLcidConstants.Denmark },
-
             { "11,509 tage", new TimeSpan(11, 12, 13, 14, 15), GlobalizationLcidConstants.Germany },
             { "12,213 stunden", new TimeSpan(0, 12, 13, 14, 15), GlobalizationLcidConstants.Germany },
             { "13,234 min", new TimeSpan(0, 0, 13, 14, 15), GlobalizationLcidConstants.Germany },
