dd3f9efb8381a371c43214bcc7f5b96a67141329
Per Kops <pko@delegate.dk>
2024-04-02 10:59:11 +0200

    feat: change -v to align with Spectre.Console latest version, which overrules -v for version

--- src/Atc.Console.Spectre/CommandConstants.cs
+++ src/Atc.Console.Spectre/CommandConstants.cs
@@ -4,8 +4,8 @@ public static class CommandConstants
 {
     public const string ArgumentShortHelp = "-h";
     public const string ArgumentLongHelp = "--help";
+    public const string ArgumentShortVersion = "-v";
     public const string ArgumentLongVersion = "--version";
-    public const string ArgumentShortVerbose = "-v";
     public const string ArgumentLongVerbose = "--verbose";
 
     public const string NameOptionsFile = "options-file";
