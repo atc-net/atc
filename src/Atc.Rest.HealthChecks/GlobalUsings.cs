179eb7a710f3f5ca424f80ab58a3bb5b45f7d11f
Per Kops <pko@delegate.dk>
2024-04-08 10:31:17 +0200

    feat: update deprecated package Microsoft.AspNetCore.Diagnostics.HealthChecks to Microsoft.Extensions.Diagnostics.HealthChecks

--- src/Atc.Rest.HealthChecks/GlobalUsings.cs
+++ src/Atc.Rest.HealthChecks/GlobalUsings.cs
@@ -1,10 +1,8 @@
 global using System.Net.Mime;
 global using System.Text.Json;
-
 global using Atc.Rest.HealthChecks.Extensions;
 global using Atc.Rest.HealthChecks.Models;
 global using Atc.Serialization;
-
 global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 global using Microsoft.AspNetCore.Http;
 global using Microsoft.Extensions.Diagnostics.HealthChecks;
