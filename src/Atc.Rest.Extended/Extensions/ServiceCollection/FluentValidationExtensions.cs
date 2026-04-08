e69d42476d0c006d580a07892ab1cfe537dc99c5
davidkallesen <david@lmdk.dk>
2026-04-09 00:51:02 +0200

    fix(atc-rest): migrate FluentValidation, structured logging and middleware ordering
    - Replace obsolete AddFluentValidation with AddFluentValidationAutoValidation
    - Use structured log properties in RequestResponseLoggerMiddleware
    - Move setupAction after core middleware for correlation ID access

--- src/Atc.Rest.Extended/Extensions/ServiceCollection/FluentValidationExtensions.cs
+++ src/Atc.Rest.Extended/Extensions/ServiceCollection/FluentValidationExtensions.cs
@@ -19,34 +19,23 @@ public static class FluentValidationExtensions
         bool useAutoRegistrateServices,
         List<AssemblyPairOptions> assemblyPairs)
     {
-        // TODO: AddFluentValidation is obsolete
-        // - this should be change to use AddFluentValidationAutoValidation instead and tested!
-        // https://github.com/FluentValidation/FluentValidation/issues/1965
-#pragma warning disable CS0618 // Type or member is obsolete
         services
-            .AddControllers()
-            .AddFluentValidation(options =>
+            .AddControllers();
+
+        services
+            .AddFluentValidationAutoValidation()
+            .AddFluentValidationClientsideAdapters()
+            .AddValidatorsFromAssemblyContaining<TStartup>();
+
+        if (useAutoRegistrateServices)
+        {
+            ArgumentNullException.ThrowIfNull(assemblyPairs);
+
+            foreach (var assemblyPairOptions in assemblyPairs)
             {
-                options.RegisterValidatorsFromAssemblyContaining<TStartup>();
-
-                if (useAutoRegistrateServices)
-                {
-                    foreach (var assemblyPairOptions in assemblyPairs)
-                    {
-                        var apiValidationTypes = assemblyPairOptions.ApiAssembly.GetValidationTypes();
-                        if (apiValidationTypes.Length > 0)
-                        {
-                            options.RegisterValidatorsFromAssemblyContaining(apiValidationTypes[0]);
-                        }
-
-                        var domainValidationTypes = assemblyPairOptions.DomainAssembly.GetValidationTypes();
-                        if (domainValidationTypes.Length > 0)
-                        {
-                            options.RegisterValidatorsFromAssemblyContaining(domainValidationTypes[0]);
-                        }
-                    }
-                }
-            });
-#pragma warning restore CS0618 // Type or member is obsolete
+                services.AddValidatorsFromAssembly(assemblyPairOptions.ApiAssembly);
+                services.AddValidatorsFromAssembly(assemblyPairOptions.DomainAssembly);
+            }
+        }
     }
 }
