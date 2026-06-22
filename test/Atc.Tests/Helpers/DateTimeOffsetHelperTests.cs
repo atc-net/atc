17bc77f3b6af5368615f3a51f4cd4c7c7ef60352
David Kallesen <david@lmdk.dk>
2026-06-22 13:13:41 +0200

    feat(atc): add UsingSpecificCulture overloads to DateTimeOffsetHelper
    Mirror the DateTimeHelper pattern: refactor existing UsingCurrentUiCulture
    methods to delegate to new UsingSpecificCulture variants. Adds:
    TryParseUsingSpecificCulture, TryParseShortDateUsingSpecificCulture,
    TryParseShortTimeUsingSpecificCulture, TryParseShortTimeUsingSpecificCultureUtc.
    Also adds ToShortDateStringUsingSpecificCulture to DateTimeOffsetExtensions.

--- test/Atc.Tests/Helpers/DateTimeOffsetHelperTests.cs
+++ test/Atc.Tests/Helpers/DateTimeOffsetHelperTests.cs
@@ -130,4 +130,98 @@ public class DateTimeOffsetHelperTests
         // Assert
         Assert.Equal(expected, actual);
     }
+
+    [Theory]
+    [InlineData(true, GlobalizationLcidConstants.UnitedStates, "10/15/2023")]
+    [InlineData(true, GlobalizationLcidConstants.UnitedStates, "10-15-2023")]
+    [InlineData(false, GlobalizationLcidConstants.UnitedStates, "20/15/2023")]
+    [InlineData(true, GlobalizationLcidConstants.GreatBritain, "15/10/2023")]
+    [InlineData(false, GlobalizationLcidConstants.GreatBritain, "15/20/2023")]
+    [InlineData(true, GlobalizationLcidConstants.Denmark, "15.10.2023")]
+    [InlineData(false, GlobalizationLcidConstants.Denmark, "15.20.2023")]
+    [InlineData(true, GlobalizationLcidConstants.Germany, "15.10.2023")]
+    [InlineData(false, GlobalizationLcidConstants.Germany, "15.20.2023")]
+    public void TryParseUsingSpecificCulture(
+        bool expected,
+        int lcid,
+        string value)
+    {
+        var actual = DateTimeOffsetHelper.TryParseUsingSpecificCulture(
+            value,
+            new CultureInfo(lcid),
+            out _);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(true, GlobalizationLcidConstants.UnitedStates, "10/15/2023")]
+    [InlineData(true, GlobalizationLcidConstants.UnitedStates, "10-15-2023")]
+    [InlineData(false, GlobalizationLcidConstants.UnitedStates, "20/15/2023")]
+    [InlineData(true, GlobalizationLcidConstants.GreatBritain, "15/10/2023")]
+    [InlineData(false, GlobalizationLcidConstants.GreatBritain, "15/20/2023")]
+    [InlineData(true, GlobalizationLcidConstants.Denmark, "15.10.2023")]
+    [InlineData(false, GlobalizationLcidConstants.Denmark, "15.20.2023")]
+    [InlineData(true, GlobalizationLcidConstants.Germany, "15.10.2023")]
+    [InlineData(false, GlobalizationLcidConstants.Germany, "15.20.2023")]
+    public void TryParseShortDateUsingSpecificCulture(
+        bool expected,
+        int lcid,
+        string value)
+    {
+        var actual = DateTimeOffsetHelper.TryParseShortDateUsingSpecificCulture(
+            value,
+            new CultureInfo(lcid),
+            out _);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(true, GlobalizationLcidConstants.UnitedStates, "3:30 AM")]
+    [InlineData(true, GlobalizationLcidConstants.UnitedStates, "3:30 PM")]
+    [InlineData(false, GlobalizationLcidConstants.UnitedStates, "3:30 X")]
+    [InlineData(true, GlobalizationLcidConstants.GreatBritain, "03:30")]
+    [InlineData(true, GlobalizationLcidConstants.GreatBritain, "15:30")]
+    [InlineData(false, GlobalizationLcidConstants.GreatBritain, "24:30")]
+    [InlineData(true, GlobalizationLcidConstants.Denmark, "03.30")]
+    [InlineData(true, GlobalizationLcidConstants.Denmark, "15.30")]
+    [InlineData(false, GlobalizationLcidConstants.Denmark, "24.30")]
+    [InlineData(true, GlobalizationLcidConstants.Germany, "03:30")]
+    [InlineData(true, GlobalizationLcidConstants.Germany, "15:30")]
+    [InlineData(false, GlobalizationLcidConstants.Germany, "24:30")]
+    public void TryParseShortTimeUsingSpecificCulture(
+        bool expected,
+        int lcid,
+        string value)
+    {
+        var actual = DateTimeOffsetHelper.TryParseShortTimeUsingSpecificCulture(
+            value,
+            new CultureInfo(lcid),
+            out _);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(true, GlobalizationLcidConstants.UnitedStates, "3:30 AM")]
+    [InlineData(true, GlobalizationLcidConstants.UnitedStates, "3:30 PM")]
+    [InlineData(false, GlobalizationLcidConstants.UnitedStates, "3:30 X")]
+    [InlineData(true, GlobalizationLcidConstants.GreatBritain, "03:30")]
+    [InlineData(true, GlobalizationLcidConstants.GreatBritain, "15:30")]
+    [InlineData(false, GlobalizationLcidConstants.GreatBritain, "24:30")]
+    [InlineData(true, GlobalizationLcidConstants.Denmark, "03.30")]
+    [InlineData(true, GlobalizationLcidConstants.Denmark, "15.30")]
+    [InlineData(false, GlobalizationLcidConstants.Denmark, "24.30")]
+    [InlineData(true, GlobalizationLcidConstants.Germany, "03:30")]
+    [InlineData(true, GlobalizationLcidConstants.Germany, "15:30")]
+    [InlineData(false, GlobalizationLcidConstants.Germany, "24:30")]
+    public void TryParseShortTimeUsingSpecificCultureUtc(
+        bool expected,
+        int lcid,
+        string value)
+    {
+        var actual = DateTimeOffsetHelper.TryParseShortTimeUsingSpecificCultureUtc(
+            value,
+            new CultureInfo(lcid),
+            out _);
+        Assert.Equal(expected, actual);
+    }
 }
